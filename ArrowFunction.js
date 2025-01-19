7c1e58f4f3364fd35f9448a463599396648c74ae
Bidhan Khatri <khatribidhan9@gmail.com>
2025-01-19 22:30:40 +0545

    Arrow function

---
+++ ArrowFunction.js
@@ -0,0 +1,11 @@
+// Arrow function is a new way to create a function
+// Syntax of arrow function is
+
+// const arrowFunction = (parameter) => { //code }
+
+//Example:
+const add = (a, b) => {
+  return a + b;
+};
+
+console.log(add(2, 3));
