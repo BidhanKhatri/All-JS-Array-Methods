5f30d85e3707f73d0f113065cfb53155acadd720
Bidhan Khatri <khatribidhan9@gmail.com>
2025-01-21 14:59:47 +0545

    concat method

---
+++ Concat.js
@@ -0,0 +1,14 @@
+// concat() function helps to merge two or more arrays
+// concat() returns a new array containing the elements of the first array followed by the elements of the second array
+
+// Syntax: array1.concat(array2);
+
+// Example:
+const array1 = [1, 2, 3, 4];
+const array2 = [5, 6, 7];
+
+const concatedArray = array1.concat(array2); //[1, 2, 3, 4, 5, 6, 7];
+console.log(concatedArray);
+
+// As a result I am getting concated array
+// concat() can be used with other data types as well like string, number, boolean
