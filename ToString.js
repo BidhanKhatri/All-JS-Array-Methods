77c52932645fcdb0885f8ca75e56bca6b5b68498
Bidhan Khatri <khatribidhan9@gmail.com>
2025-01-21 22:09:43 +0545

    toString() method

---
+++ ToString.js
@@ -0,0 +1,9 @@
+// toString() helps to convert array into string
+// toString() returns a string
+
+// Syntax: array.toString();
+
+const data = ["Mango", 2, "Ram", "Shyam"];
+
+const changeToString = data.toString();
+console.log(changeToString); // Mango,2,Ram,Shyam
