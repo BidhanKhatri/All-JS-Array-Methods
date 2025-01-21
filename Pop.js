803a01d7ba2610fddbea648d3a0757b61694c6ea
Bidhan Khatri <khatribidhan9@gmail.com>
2025-01-21 21:51:06 +0545

    pop() method

---
+++ Pop.js
@@ -0,0 +1,11 @@
+// pop() method is used to remove the last element of an array
+// pop() returns the removed element
+
+// Syntax: array.pop();
+
+const array = [4, "Bidhan", "Nepal", "Pokhara"];
+
+const removedElement = array.pop();
+
+console.log(removedElement); // "Pokhara"
+console.log(array); // [4, "Bidhan", "Nepal"]
