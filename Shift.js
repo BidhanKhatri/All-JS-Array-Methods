ab7480d04b67b98cec12b1c13d09902301a2ac7b
Bidhan Khatri <khatribidhan9@gmail.com>
2025-01-21 21:53:49 +0545

    shift() method

---
+++ Shift.js
@@ -0,0 +1,11 @@
+// shift() method is used to remove the first element of an array
+// shift() returns the removed element
+
+// Syntax: array.shift();
+
+const array = [4, "Bidhan", "Nepal", "Pokhara"];
+
+const removedElement = array.shift();
+
+console.log(removedElement); //4
+console.log(array); // ["Bidhan", "Nepal", "Pokhara"]
