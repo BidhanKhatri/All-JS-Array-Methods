8009b9ef42ab8ea24c216f76ed4188afc1db1e88
Bidhan Khatri <khatribidhan9@gmail.com>
2025-01-22 12:51:38 +0545

    valueOf() method

---
+++ ValueOf.js
@@ -0,0 +1,9 @@
+// valueOf() method is used to copy the array 
+
+// Syntax: array.valueOf();
+
+const data = ["Ram", "Shyam", 18, 19];
+console.log("Original Array: ", data); // [ 'Ram', 'Shyam', 18, 19 ]
+
+const copiedData = data.valueOf(); 
+console.log("Copied Array: ", copiedData); // [ 'Ram', 'Shyam', 18, 19 ]
