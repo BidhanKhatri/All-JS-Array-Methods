de8c3acb247c694f23642144e776aa8e140b7b0d
Bidhan Khatri <khatribidhan9@gmail.com>
2025-01-22 12:23:15 +0545

    reverse() method

---
+++ Reverse.js
@@ -0,0 +1,9 @@
+// reverse() method is used to reverse the elements of an array
+
+// Syntax: array.reverse();
+
+const array = [1, 2, 3, 4, 5];
+const names = ["ram", "shyam", "hari", "geta"];
+
+console.log(array.reverse()); // [5, 4, 3, 2, 1]
+console.log(names.reverse()); // ["geta", "hari", "shyam", "ram"]
